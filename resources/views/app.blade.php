d9190ecd1df4c5be6cae3acf8d7dcce8ac43d092
Sardar IT <sardaritbd.official@gmail.com>
2025-12-14 11:22:44 +0600

    readmi updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <!-- ADD THIS -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Nexo Travel') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
